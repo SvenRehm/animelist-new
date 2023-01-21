0d9613fac5a58a4082ae583ba32cac6717d6712d
SvenRehm <rehm21@web.de>
2023-01-21 19:31:30 +0100

    routing, first styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react"
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react"
+import Layout from "../components/layout"
+import { AppProps } from "next/app"
+import "../styles/globals.css"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+   Component,
+   pageProps,
+}: AppProps<{
+   initialSession: Session
+}>) {
+   const [supabase] = useState(() => createBrowserSupabaseClient())
+
+   return (
+      <SessionContextProvider
+         supabaseClient={supabase}
+         initialSession={pageProps.initialSession}
+      >
+         <Layout>
+            <Component {...pageProps} />
+         </Layout>
+      </SessionContextProvider>
+   )
 }
+export default MyApp
